38a5127b1c9d9342d3672bf6aa73c413c2017e91
Craig Adey <>
2018-09-26 19:18:21 +0100

    initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+ 
+  it('does 1 equal 2', async(() => {
+    expect(1).toBe(2);
+  }));
+});
